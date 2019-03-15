326789cd674e499ddd5a25d4b1029f4d4d12914d
Caner Ozer <caneozester@gmail.com>
2019-03-15 14:40:58 +0300

    fig is added in order to make it possible to save the image, when the kwarg is fed

--- visualize.py
+++ visualize.py
@@ -75,7 +75,7 @@ def apply_mask(image, mask, color, alpha=0.5):
 
 def display_instances(image, boxes, masks, class_ids, class_names,
                       scores=None, title="",
-                      figsize=(16, 16), ax=None):
+                      figsize=(16, 16), ax=None, savefig=False):
     """
     boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
     masks: [height, width, num_instances]
@@ -92,7 +92,9 @@ def display_instances(image, boxes, masks, class_ids, class_names,
         assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
 
     if not ax:
-        _, ax = plt.subplots(1, figsize=figsize)
+        fig, ax = plt.subplots(1, figsize=figsize)
+    else:
+        fig, ax = ax[0], ax[1]
 
     # Generate random colors
     colors = random_colors(N)
@@ -144,6 +146,8 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
     plt.show()
+    if savefig:
+        fig.savefig(title+".png")
     
 
 def draw_rois(image, rois, refined_rois, mask, class_ids, class_names, limit=10):
